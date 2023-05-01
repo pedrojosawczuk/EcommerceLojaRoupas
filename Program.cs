bad239d61f1999dbc5fded642f9636c91ed863e9
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-05-01 15:12:14 -0300

    change: namespace, add welcome screen

--- Program.cs
+++ Program.cs
@@ -1,73 +1,75 @@
 ﻿using System.Collections.Generic;
 using System;
 using System.Runtime.InteropServices;
+using ecommerceLojaRoupas.Model;
 
-namespace Projeto_loja_Virtual
+namespace ecommerceLojaRoupas;
+
+class Program
 {
-    class Program
+    static List<Produto> listaProdutos = new List<Produto>();
+    static List<CategoriaProduto> listaCategorias = new List<CategoriaProduto>();
+    static List<Cliente> listaClientes = new List<Cliente>();
+    static List<Venda> listaVendas = new List<Venda>();
+
+    static void Main(string[] args)
     {
-        static List<Produto> listaProdutos = new List<Produto>();
-        static List<CategoriaProduto> listaCategorias = new List<CategoriaProduto>();
-        static List<Cliente> listaClientes = new List<Cliente>();
-        static List<Venda> listaVendas = new List<Venda>();
-
-        static void Main(string[] args)
+        int opcao = 0;
+        do
         {
-            int opcao = 0;
-            do
+            Console.Clear();
+            Console.WriteLine(" 🛍️ e-commerce do Jão 👗 ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine(" 1 - Cadastrar Categoria");
+            Console.WriteLine(" 2 - Listar Categorias");
+            Console.WriteLine(" 3 - Cadastrar Produto");
+            Console.WriteLine(" 4 - Listar Produtos");
+            Console.WriteLine(" 5 - Cadastrar Cliente");
+            Console.WriteLine(" 6 - Listar Clientes");
+            Console.WriteLine(" 7 - Registrar Venda");
+            Console.WriteLine(" 8 - Listar Vendas");
+            Console.WriteLine(" 0 - Sair");
+            opcao = int.Parse(Console.ReadLine() ?? "");
+
+            switch (opcao)
             {
-                Console.Clear();
-                Console.WriteLine("Escolha uma opção:");
-                Console.WriteLine("1 - Cadastrar Categoria");
-                Console.WriteLine("2 - Listar Categorias");
-                Console.WriteLine("3 - Cadastrar Produto");
-                Console.WriteLine("4 - Listar Produtos");
-                Console.WriteLine("5 - Cadastrar Cliente");
-                Console.WriteLine("6 - Listar Clientes");
-                Console.WriteLine("7 - Registrar Venda");
-                Console.WriteLine("8 - Listar Vendas");
-                Console.WriteLine("0 - Sair");
-                opcao = int.Parse(Console.ReadLine());
-
-                switch (opcao)
-                {
-                    case 1:
+                case 1:
                         .CadastrarCategoria(listaCategorias);
-                        break;
-                    case 2:
+                    break;
+                case 2:
                         .ListarCategorias(listaCategorias);
-                        break;
-                    case 3:
+                    break;
+                case 3:
                         .CadastrarProduto(listaProdutos, listaCategorias);
-                        break;
-                    case 4:
+                    break;
+                case 4:
                         .ListarProdutos(listaProdutos);
-                        break;
-                    case 5:
+                    break;
+                case 5:
                         .CadastrarCliente(listaClientes);
-                        break;
-                    case 6:
+                    break;
+                case 6:
                         .ListarClientes(listaClientes);
-                        break;
-                    case 7:
+                    break;
+                case 7:
                         .CadastrarVenda(listaVendas, listaClientes, listaProdutos);
-                        break;
-                    case 8:
+                    break;
+                case 8:
                         .ListarVenda(listaVendas);
-                        break;
-                    case 0:
-                        Console.WriteLine("Programa encerrado.");
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida. Digite novamente.");
-                        break;
-                }
+                    break;
+                case 0:
+                    Console.WriteLine(" Programa encerrado.");
+                    break;
+                default:
+                    Console.WriteLine(" Opção inválida. Digite novamente.");
+                    break;
+            }
 
 
-                Console.WriteLine("\nPressione qualquer tecla para continuar...");
-                Console.ReadKey();
+            Console.WriteLine("\n Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
 
-            } while (opcao != 0);
-        }
+        } while (opcao != 0);
     }
 }
