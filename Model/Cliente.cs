1a7ae60a7c18d1e2ee159a4cbf0837ef8e5cb07a
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-05-01 15:05:41 -0300

    change: Cliente.cs private to public attributes

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -1,47 +1,11 @@
 namespace ecommerceLojaRoupas.Model;
 
-// classe Cliente
 class Cliente
 {
-    public int ClienteID;
-    private string _nome;
-    private string _email;
-
-    public Cliente(int id, string nome, string email)
-    {
-        this.ClienteID = id;
-        this._nome = nome;
-        this._email = email;
-    }
-
-    public string Nome
-    {
-        get { return _nome; }
-        set
-        {
-            try
-            {
-                this._nome = value;
-            }
-            catch (NullReferenceException ex)
-            {
-                Console.WriteLine(" ❌ ERROR: " + ex.Message + " ❌ ");
-            }
-        }
-    }
-    public string Email
-    {
-        get { return _email; }
-        set
-        {
-            try
-            {
-                this._email = value;
-            }
-            catch (NullReferenceException ex)
-            {
-                Console.WriteLine(" ❌ ERROR: " + ex.Message + " ❌ ");
-            }
-        }
-    }
+    public long ClienteID;
+    public string Nome = string.Empty;
+    public string Sobrenome = string.Empty;
+    public string Email = string.Empty;
+    public string Telefone = string.Empty;
+    public string Endereco = string.Empty;
 }
