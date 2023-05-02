f471672a16a3ec1af957412772f1e5cfbfdeff5e
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-05-02 11:16:09 -0300

    add: ClienteUI.cs

---
+++ UI/ClienteUI.cs
@@ -0,0 +1,41 @@
+using ecommerceLojaRoupas.Model;
+
+namespace ecommerceLojaRoupas.UI;
+
+//criar Clientes e Listar Clientes
+class ClienteUI
+{
+    public static void CadastrarCliente(List<Cliente> listaClientes)
+    {
+        Console.Clear();
+        Console.WriteLine("CADASTRO DE CLIENTE\n");
+
+        Console.Write("Nome do cliente: ");
+        string nome = Console.ReadLine() ?? "";
+
+        Console.Write("E-mail do cliente: ");
+        string email = Console.ReadLine() ?? "";
+
+        int id = listaClientes.Count + 1;
+
+        Cliente cliente = new Cliente(id, nome, email);
+
+        listaClientes.Add(cliente);
+
+        Console.WriteLine("\nCliente cadastrado com sucesso!");
+    }
+
+    public static void ListarClientes(List<Cliente> listaClientes)
+    {
+        Console.Clear();
+        Console.WriteLine("LISTAGEM DE CLIENTES\n");
+
+        foreach (Cliente cliente in listaClientes)
+        {
+            Console.WriteLine("ID: " + cliente.Id);
+            Console.WriteLine("Nome: " + cliente.Nome);
+            Console.WriteLine("E-mail: " + cliente.Email);
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
